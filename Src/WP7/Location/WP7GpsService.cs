c21b5941a15d31b995af8d6acaee5ce38625fb5d
kamran <ksaleem77@hotmail.com>
2011-07-01 10:58:08 +0200

    added WPSimulatorLocation class for simulator location

--- Src/WP7/Location/WP7GpsService.cs
+++ Src/WP7/Location/WP7GpsService.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿//#define GPS_EMULATOR
+using System;
 using System.Device.Location;
+using GpsEmulatorClient.ServiceReference1;
 
 namespace Applicable.Location
 {
     public class Wp7LocationService : ILocationService
     {
 
-        private readonly GeoCoordinateWatcher _watcher;
-
+        //private readonly GeoCoordinateWatcher _watcher;
+        private readonly IGeoPositionWatcher<GeoCoordinate> _watcher;
         public Wp7LocationService()
         {
-            _watcher = new GeoCoordinateWatcher();
-            _watcher.PositionChanged += PositionChanged;
+            //_watcher = new GeoCoordinateWatcher();
+           // #if GPS_EMULATOR
+               // _watcher = new GpsEmulatorClient.GeoCoordinateWatcher();
+           // #else
+                _watcher = new System.Device.Location.GeoCoordinateWatcher();
+           // #endif
+
+            //_watcher.PositionChanged += PositionChanged;
+                _watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(PositionChanged);
         }
 
         void PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
@@ -41,6 +50,17 @@ namespace Applicable.Location
             var longtitude = position.Location.Longitude;
             var heading = 0d;
             var accuracy = position.Location.HorizontalAccuracy;
+            //double latitude = 63.425630295;
+            //double longitude = 10.4458852325;
+            //double heading = 0d;
+            //double accuracy = 0.0;
+
+            //if (!position.Location.IsUnknown)
+            //{ 
+            //    latitude = position.Location.Latitude;
+            //    longitude = position.Location.Longitude;
+            //    accuracy = position.Location.HorizontalAccuracy;
+            //}
             
             return new LocationData(latitude, longtitude, heading, accuracy, position.Timestamp.LocalDateTime);        
         }
---
+++ Src/WP7/Location/WP7SimulatorLocation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Applicable.Location
+{
+    
+    public class WP7SimulatorLocation : ILocationService
+    {
+        DispatcherTimer _timer;
+        double _longtitude;
+        double _latitude;
+        bool _running;
+
+        public WP7SimulatorLocation()
+        {
+            _latitude = 63.425630295;
+            _longtitude = 10.4458852325;
+           // _timer = new DispatcherTimer();
+           // _timer.Interval = TimeSpan.FromMinutes(1);
+           // _timer.Tick += new EventHandler(timer_Tick);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            if (_running)
+            {
+                var loc = new LocationData(_latitude, _longtitude, 0, 1, DateTime.Now);
+                if (LocationChanged != null)
+                {
+                    LocationChanged(loc);
+                }
+                _latitude += 0.000001;
+                _longtitude += 0.000001;
+            }
+        }
+
+        private void SetLocation()
+        {
+            //throw new NotImplementedException();
+            if (_running)
+            {
+                var loc = new LocationData(_latitude, _longtitude, 0, 1, DateTime.Now);
+                if (LocationChanged != null)
+                {
+                    LocationChanged(loc);
+                }
+                _latitude += 0.000001;
+                _longtitude += 0.000001;
+            }
+        }
+
+        public void Start()
+        {
+            _running = true;
+            SetLocation();
+        }
+
+        public void Stop()
+        {
+            _running = false;
+        }
+
+        public Action<LocationData> LocationChanged
+        {
+            get;
+            set;
+        }
+    }
+}
